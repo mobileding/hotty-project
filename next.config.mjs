411b7e543a29278aafd4785260d098e7e2f64bbb
mobileding <mobileding@gmail.com>
2025-11-21 21:38:37 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@runpod/api'],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
 };
 
